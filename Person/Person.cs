ae67570dcf6b644248a79df3db95f9874e35c951
inawashiro <hiroina0926@icloud.com>
2019-07-29 22:26:56 +0900

    Initial check-in of module Person

---
+++ Person/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace Person
+{
+    public class Person
+    {
+        private string Name;
+        private int Age;
+
+        //constructor
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        //constructor
+        public Person(string name) : this(name, -1) { }
+
+        //constructor
+        public Person(int age) : this("None", age) { }
+
+        //constructor
+        public Person() : this("None", -1) { }
+
+        public string GetName()
+        {
+            return this.Name;
+        }
+
+        public void SetName(string name)
+        {
+            this.Name = name;
+        }
+
+        public int GetAge()
+        {
+            return this.Age;
+        }
+
+        public void SetAge(int age)
+        {
+            this.Age = age > 0 ? age : 0;
+        }
+    }
+}
---
+++ Person/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Person
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person("Hiromichi", 24);
+            Console.WriteLine($"{person.GetName()} is {person.GetAge()} years old.");
+
+            person = new Person("Hiromichi");
+            Console.WriteLine($"{person.GetName()} is {person.GetAge()} years old.");
+
+            person = new Person(24);
+            Console.WriteLine($"{person.GetName()} is {person.GetAge()} years old.");
+
+            person = new Person();
+            Console.WriteLine($"{person.GetName()} is {person.GetAge()} years old.");
+        }
+    }
+}
